85607f1de9d0906828116d709a7bb445e4f91d2a
Harry Mellsop <harrymellsop@gmail.com>
2020-04-01 09:48:41 +1300

    Why do I always add node modules and waste 30 minutes unpicking the commits:

---
+++ main.js
@@ -0,0 +1,62 @@
+const { app, BrowserWindow } = require('electron')
+var path = require('path')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    //icon: path.join(__dirname, 'assets/icons/png/64x64.png') (uncomment me when we need a favicon)
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('src/index.html')
+
+  // Open the DevTools (if required)
+  win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    
+    // keep it this way for easier debugging; change it back
+    // for standard macOS behaviour
+    
+    //win = null
+    app.quit()
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
